d08506ff504548b42f580794fe776e603f458187
santiago velez saffon <santipro@santiagos-MacBook-Pro.local>
2025-04-12 07:37:13 -0500

    Add supabase middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Hero from "@/components/hero";
 
 export default async function Home() {
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
         Hello
